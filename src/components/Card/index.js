b81788700035ee7eca5930e998e1ad3a8c3c0ff7
Shiro Neko <tagiki5@gmail.com>
2020-09-01 20:43:43 -0300

    Estruturação da interface (falta estilização)

---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+
+      <p>Fazer a migração completa do servidor.</p>
+      <img src="https://avatars1.githubusercontent.com/u/47436525?s=460&u=358490529c38d8db8f6afff029ba8d0af1dec601&v=4" alt="Avatar"/>
+    </Container>
+  )
+}
---
+++ src/components/Card/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  img {
+    width: 24px;
+    height: 24px;
+    border-radius: 54px;
+    margin-top: 5px;
+  }
+`;
+
+export const Label = styled.span``;
