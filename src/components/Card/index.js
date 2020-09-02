6ce5814f20ebd71a6d2de7936c0518bbcca151b6
Shiro Neko <tagiki5@gmail.com>
2020-09-01 22:54:37 -0300

    Estilização Básica concluída

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <Container>
-      <header>
-        <Label color="#7159c1" />
-      </header>
+      {data.labels.map(label => <Label key={label} color={label} />)}
 
-      <p>Fazer a migração completa do servidor.</p>
-      <img src="https://avatars1.githubusercontent.com/u/47436525?s=460&u=358490529c38d8db8f6afff029ba8d0af1dec601&v=4" alt="Avatar"/>
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt=""/>}
     </Container>
-  )
+  );
 }
