18d4d2c226d4fe93d63a7cca14fb5b6513231a16
Shiro Neko <tagiki5@gmail.com>
2021-01-04 19:30:20 -0300

    Updates

---
+++ precache-manifest.5f75220689775a365fb7ffc3605e9a99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807e628031d172103b17b44d1726024d",
+    "url": "/interface-pipefy/index.html"
+  },
+  {
+    "revision": "2c54f3ad6a9f61a9d71e",
+    "url": "/interface-pipefy/static/js/2.9e3531a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/interface-pipefy/static/js/2.9e3531a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c3180e4da2b2d83613b",
+    "url": "/interface-pipefy/static/js/main.e96f5b76.chunk.js"
+  },
+  {
+    "revision": "5c94194b66a392cc0a11",
+    "url": "/interface-pipefy/static/js/runtime-main.5bee838f.js"
+  }
+]);
